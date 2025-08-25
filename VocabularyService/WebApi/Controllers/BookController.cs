f551893820e9c0dc2e10e8edeb9afed33d6a24a1
Sweet <sweetlighthousevn@gmail.com>
2025-08-25 11:31:55 +0700

    Create branch for pmtben to fix.

--- VocabularyService/WebApi/Controllers/BookController.cs
+++ VocabularyService/WebApi/Controllers/BookController.cs
@@ -4,93 +4,112 @@ using Application.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
-namespace WebApi.Controllers
+namespace WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BookController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BookController : ControllerBase
+    private readonly IBookService _bookService;
+
+    public BookController(IBookService bookService)
     {
-        private readonly IBookService _bookService;
+        _bookService = bookService;
+    }
 
-        public BookController(IBookService bookService)
-        {
-            _bookService = bookService;
-        }
+    // GET: api/book
+    [Authorize(Roles = "admin")]
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<BookDto>>> GetAllBooks()
+    {
+        var books = await _bookService.GetAllBooksAsync();
+        return Ok(books);
+    }
 
-        // GET: api/book
-        [Authorize(Roles = "admin")]
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<BookDto>>> GetAllBooks()
+    // GET: api/book/{id}
+    [Authorize(Roles = "user")]
+    [HttpGet("{id}")]
+    public async Task<ActionResult<BookDto>> GetBookById(Guid id)
+    {
+        Claim? userIdClaim = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+        Guid userId = Guid.Parse(userIdClaim!.Value);
+        try
         {
-            var books = await _bookService.GetAllBooksAsync();
-            return Ok(books);
-        }
-
-        // GET: api/book/{id}
-        [Authorize(Roles = "user")]
-        [HttpGet("{id}")]
-        public async Task<ActionResult<BookDto>> GetBookById(Guid id)
-        {
-            try
+            var book = await _bookService.GetBookByIdAsync(id);
+            // kiem tra book nay co của nguoi do hay ko
+            if (book.UserId != userId)
             {
-                var book = await _bookService.GetBookByIdAsync(id);
-                return Ok(book);
+                return Unauthorized("You cant see this book if you dont own it.");
             }
-            catch (KeyNotFoundException ex)
-            {
-                return NotFound(ex.Message);
-            }
+            return Ok(book);
+            
         }
-
-        // GET: api/book/user/{userId}
-        [HttpGet("user/{userId}")]
-        [Authorize(Roles = "user")]
-        public async Task<ActionResult<IEnumerable<BookOfUserDto>>> GetBooksByUserId(Guid userId)
+        catch (KeyNotFoundException ex)
         {
-            var books = await _bookService.GetAllBooksByUserIdAsync(userId);
-            return Ok(books);
+            return NotFound(ex.Message);
         }
+    }
 
-        // POST: api/book
-        [Authorize(Roles = "user")]
-        [HttpPost]
-        public async Task<ActionResult> AddBook([FromBody] BookCreateDto bookCreateDto)
+    // GET: api/book/user/{userId}
+    [HttpGet("user/{userId}")]
+    [Authorize(Roles = "user")]
+    public async Task<ActionResult<IEnumerable<BookOfUserDto>>> GetBooksByUserId(Guid userId)
+    {
+        var books = await _bookService.GetAllBooksByUserIdAsync(userId);
+        return Ok(books);
+    }
+
+    [HttpGet("my-book")]
+    [Authorize(Roles = "user")]
+    public async Task<ActionResult<IEnumerable<BookOfUserDto>>> GetMyBook()
+    {
+        Claim? userIdClaim = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+        Guid userId = Guid.Parse(userIdClaim!.Value);
+
+        var books = await _bookService.GetAllBooksByUserIdAsync(userId);
+        return Ok(books);
+    }
+
+    // POST: api/book
+    [Authorize(Roles = "user")]
+    [HttpPost]
+    public async Task<ActionResult> AddBook([FromBody] BookCreateDto bookCreateDto)
+    {
+        await _bookService.AddBookAsync(bookCreateDto);
+        return Ok(new { message = "Book added successfully" });
+    }
+
+    // PUT: api/book
+    [Authorize(Roles = "user")]
+    [HttpPut]
+    public async Task<ActionResult> UpdateBook([FromBody] BookUpdateDto bookUpdateDto)
+    {
+        try
         {
-            await _bookService.AddBookAsync(bookCreateDto);
-            return Ok(new { message = "Book added successfully" });
+            await _bookService.UpdateBookAsync(bookUpdateDto);
+            return Ok(new { message = "Book updated successfully" });
         }
-
-        // PUT: api/book
-        [Authorize(Roles = "user")]
-        [HttpPut]
-        public async Task<ActionResult> UpdateBook([FromBody] BookUpdateDto bookUpdateDto)
+        catch (KeyNotFoundException ex)
         {
-            try
-            {
-                await _bookService.UpdateBookAsync(bookUpdateDto);
-                return Ok(new { message = "Book updated successfully" });
-            }
-            catch (KeyNotFoundException ex)
-            {
-                return NotFound(ex.Message);
-            }
+            return NotFound(ex.Message);
         }
+    }
 
-        // DELETE: api/book/{id}
-        [Authorize(Roles = "user")]
-        [HttpDelete("{id}")]
-        public async Task<ActionResult> DeleteBook(Guid id)
+    // DELETE: api/book/{id}
+    [Authorize(Roles = "user")]
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteBook(Guid id)
+    {
+        try
         {
-            try
-            {
-                await _bookService.DeleteBookAsync(id);
-                return Ok(new { message = "Book deleted successfully" });
-            }
-            catch (KeyNotFoundException ex)
-            {
-                return NotFound(ex.Message);
-            }
+            await _bookService.DeleteBookAsync(id);
+            return Ok(new { message = "Book deleted successfully" });
+        }
+        catch (KeyNotFoundException ex)
+        {
+            return NotFound(ex.Message);
         }
     }
 }
