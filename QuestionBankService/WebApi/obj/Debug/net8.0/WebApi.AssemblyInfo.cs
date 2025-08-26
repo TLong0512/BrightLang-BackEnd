fd1cf303d847349a7f877da4a9d4dd44e7870769
Long Nguyen Thanh <132772325+TLong0512@users.noreply.github.com>
2025-08-26 18:41:31 +0700

    push questionbank

--- QuestionBankService/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ QuestionBankService/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ee40f8083af5a565ffbc2f69e93e85017da658d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da2c621bc0f6b93a2850c5826e7de216aeebe200")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
