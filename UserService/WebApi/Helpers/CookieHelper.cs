de9b44374dde8a6faffa306e054c976ef1207a42
Sweet <sweetlighthousevn@gmail.com>
2025-09-05 16:59:35 +0700

    Fix logout in UserService

--- UserService/WebApi/Helpers/CookieHelper.cs
+++ UserService/WebApi/Helpers/CookieHelper.cs
@@ -2,34 +2,63 @@
 
 public static class CookieHelper
 {
+    private static TimeSpan RefreshTokenLifetime => TimeSpan.FromDays(7);
+    private static TimeSpan AccessTokenLifetime => TimeSpan.FromMinutes(10);
+
+    private static CookieOptions GenerateRefreshCookieOptions()
+    {
+        return new CookieOptions
+        {
+            HttpOnly = true,
+            Secure = true,
+            SameSite = SameSiteMode.None,
+            Path = "/api/Authentication"
+        };
+    }
+
+    private static CookieOptions GenerateAccessCookieOptions()
+    {
+        return new CookieOptions
+        {
+            HttpOnly = true,
+            Secure = true,
+            SameSite = SameSiteMode.None,
+            Path = "/"
+        };
+    }
+
     public static void SetAuthCookies(
         HttpResponse response,
         string accessToken,
         string refreshToken,
         Guid refreshTokenId)
     {
-        DateTime refreshTokenLifetime = DateTime.UtcNow.AddDays(7);
-        DateTime accessTokenLifetime = DateTime.UtcNow.AddMinutes(10);
+        CookieOptions refreshOptions = GenerateRefreshCookieOptions();
+        refreshOptions.Expires = DateTime.UtcNow.Add(RefreshTokenLifetime);
+        refreshOptions.MaxAge = RefreshTokenLifetime;
 
-        var refreshOptions = new CookieOptions
-        {
-            HttpOnly = true,
-            Secure = true,
-            SameSite = SameSiteMode.None,
-            Expires = refreshTokenLifetime,
-            Path = "/api/Authentication"
-        };
+        CookieOptions accessOptions = GenerateAccessCookieOptions();
+        accessOptions.Expires = DateTime.UtcNow.Add(AccessTokenLifetime);
+        accessOptions.MaxAge = AccessTokenLifetime;
 
         response.Cookies.Append(Constant.RefreshTokenName, refreshToken, refreshOptions);
         response.Cookies.Append(Constant.RefreshTokenIdName, refreshTokenId.ToString(), refreshOptions);
-
-        response.Cookies.Append(Constant.AccessTokenName, accessToken, new CookieOptions
-        {
-            HttpOnly = true,
-            Secure = true,
-            SameSite = SameSiteMode.None,
-            Expires = accessTokenLifetime,
-            Path = "/"
-        });
+        response.Cookies.Append(Constant.AccessTokenName, accessToken, accessOptions);
     }
+
+    public static void RemoveAuthCookies(HttpResponse response)
+    {
+        CookieOptions refreshOptions = GenerateRefreshCookieOptions();
+        refreshOptions.Expires = DateTime.UtcNow.AddDays(-1);
+        //refreshOptions.MaxAge = TimeSpan.FromMinutes(-1);
+
+        CookieOptions accessOptions = GenerateAccessCookieOptions();
+        accessOptions.Expires = DateTime.UtcNow.AddDays(-1);
+        //accessOptions.MaxAge = TimeSpan.FromMinutes(-1);
+
+        response.Cookies.Delete(Constant.RefreshTokenIdName, refreshOptions);
+        response.Cookies.Delete(Constant.RefreshTokenName, refreshOptions);
+        response.Cookies.Delete(Constant.AccessTokenName, accessOptions);
+    }
+
 }
