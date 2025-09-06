d5cb31c9ce0d32f4d4a3544031cb55d0c07ad3a2
Long Nguyen Thanh <132772325+TLong0512@users.noreply.github.com>
2025-09-07 00:05:07 +0700

    modify add test answer

--- TestService/Infrastructure/Repositories/Implementations/TestAnswerRepository.cs
+++ TestService/Infrastructure/Repositories/Implementations/TestAnswerRepository.cs
@@ -1,9 +1,11 @@
 ﻿using Domain.Entities;
 using Infrastructure.Contexts;
 using Infrastructure.Repositories.Intefaces;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,6 +16,14 @@ namespace Infrastructure.Repositories.Implementations
         public TestAnswerRepository(DefaultContext context) : base(context)
         {
 
+        }
+
+        public Task DeleteByCondition(Expression<Func<TestAnswer, bool>> predicate)
+        {
+            _context.TestAnswers
+                       .Where(predicate)
+                       .ExecuteUpdateAsync(s => s.SetProperty(e => e.IsDeleted, true));
+            return Task.CompletedTask;
         }
     }
 }
